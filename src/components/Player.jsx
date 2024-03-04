b9908f41ff6a61fa419453de8472ad6c38dd0819
Yashi Upadhyay <yashi.upadhyay@unthinkable.co>
2024-03-04 12:47:31 +0530

    use of ref hook

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+import { useRef } from 'react';
+
 export default function Player() {
+  let playerName = useRef();
+  let [enteredPlayerName, setName] = useState();
+
+  function handleClick() {
+    setName(playerName.current.value);
+    playerName.current.value = '';
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
