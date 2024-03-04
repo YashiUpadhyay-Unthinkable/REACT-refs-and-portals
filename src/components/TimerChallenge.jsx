6c4cedd1efd088324209b6742c0f6337059af703
Yashi Upadhyay <yashi.upadhyay@unthinkable.co>
2024-03-04 23:56:30 +0530

    use of forwardRef method

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,13 +1,17 @@
 import { useRef, useState } from 'react';
+import ResultModal from './ResultModal';
 
 export default function TimerChallenge({ title, targetTime }) {
   let [timeStarted, setTimeStarted] = useState();
   let [timeExpired, setTimeExpired] = useState();
   let timerRef = useRef();
+  let dialog = useRef();
 
   function handleStart() {
     timerRef.current = setTimeout(() => {
       setTimeExpired(true);
+      console.log(dialog);
+      dialog.current.showModal();
     }, targetTime * 1000);
     setTimeStarted(true);
   }
@@ -17,21 +21,23 @@ export default function TimerChallenge({ title, targetTime }) {
   }
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timeExpired && <p>You Lost!</p>}
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? 's' : ''}
-      </p>
-      <p>
-        <button onClick={timeStarted ? handleStop : handleStart}>
-          {timeStarted ? 'Stop' : 'Start'} challenge
-        </button>
-      </p>
-      <p className={timeStarted ? 'active' : undefined}>
-        {' '}
-        {timeStarted ? 'Time is running...' : 'Timer inactive'}
-      </p>
-    </section>
+    <>
+      <ResultModal ref={dialog} targetTime={targetTime} result="Lost" />
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+          <button onClick={timeStarted ? handleStop : handleStart}>
+            {timeStarted ? 'Stop' : 'Start'} challenge
+          </button>
+        </p>
+        <p className={timeStarted ? 'active' : undefined}>
+          {' '}
+          {timeStarted ? 'Time is running...' : 'Timer inactive'}
+        </p>
+      </section>
+    </>
   );
 }
